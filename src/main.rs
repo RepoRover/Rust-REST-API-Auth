bca846dfbd1563289bc1f19d77f4e3a69b0d215c
RepoRover <web.yugaitrov@gmail.com>
2024-11-02 17:01:53 +0200

    Server instance created

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder};
+
+async fn say_hello() -> impl Responder {
+    "Hello World!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let server: actix_web::dev::Server =
+        HttpServer::new(|| App::new().route("/signup", web::get().to(say_hello)))
+            .bind("127.0.0.1:3000")?
+            .run();
+
+    println!("Server is listening to incoming requests...");
+
+    server.await
 }
