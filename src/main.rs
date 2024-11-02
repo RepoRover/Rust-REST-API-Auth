c966b8b7903ce10f7a17ee3ad4c1f429abb6dec8
RepoRover <web.yugaitrov@gmail.com>
2024-11-02 17:15:52 +0200

    Defined all server routes provided in API documentation

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
 use actix_web::{web, App, HttpServer, Responder};
 
-async fn say_hello() -> impl Responder {
-    "Hello World!"
+async fn signup() -> impl Responder {
+    "signup"
+}
+
+async fn login() -> impl Responder {
+    "login"
+}
+
+async fn logout() -> impl Responder {
+    "logout"
+}
+
+async fn delete_user() -> impl Responder {
+    "delete_user"
+}
+
+async fn renew_user_session() -> impl Responder {
+    "renew_user_session"
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let server: actix_web::dev::Server =
-        HttpServer::new(|| App::new().route("/signup", web::get().to(say_hello)))
-            .bind("127.0.0.1:3000")?
-            .run();
+    let server: actix_web::dev::Server = HttpServer::new(|| {
+        App::new()
+            .route("/signup", web::post().to(signup))
+            .route("/login", web::post().to(login))
+            .route("/logout", web::put().to(logout))
+            .route("/user", web::delete().to(delete_user))
+            .route("/session", web::put().to(renew_user_session))
+    })
+    .bind("127.0.0.1:3000")?
+    .run();
 
     println!("Server is listening to incoming requests...");
 
